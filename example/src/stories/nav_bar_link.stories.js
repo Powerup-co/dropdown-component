516f4b9d6f494d412ff07ef2dc9639405ed45b2b
Slavik Lutsenko <slavka.com.ua@gmail.com>
2021-03-21 13:30:30 +0200

    update nav-bar link story

--- example/src/stories/nav_bar_link.stories.js
+++ example/src/stories/nav_bar_link.stories.js
@@ -8,34 +8,8 @@ export default {
     title: 'Nav Bar Link',
     component: NavBarLinkComponent,
     argTypes: {
-        path: createArgTypes({
-            description: 'Link\'s href. If the field is empty, then instead of a link (<a />), a paragrop is rendered (<p />)',
-            typeName: 'String',
-            defaultValue: NavBarLinkComponent?.defaultProps?.path,
-        }),
         text: createArgTypes({
-            typeName: 'String',
-            defaultValue: NavBarLinkComponent?.defaultProps?.text,
-        }),
-        className: createArgTypes({
-            typeName: 'String',
-            defaultValue: NavBarLinkComponent?.defaultProps?.className,
             controlType: 'text',
-        }),
-        isBold: createArgTypes({
-            typeName: 'Boolean',
-            defaultValue: NavBarLinkComponent?.defaultProps?.isBold,
-            controlType: 'boolean',
-        }),
-        withNotificationDot: createArgTypes({
-            typeName: 'Boolean',
-            defaultValue: NavBarLinkComponent?.defaultProps?.withNotificationDot,
-            controlType: 'boolean',
-        }),
-        forceUseDefaulLinkTag: createArgTypes({
-            description: 'If you are not using react-router or want the link to work as usual, then set to "True"',
-            typeName: 'Boolean',
-            defaultValue: NavBarLinkComponent?.defaultProps?.forceUseDefaulLinkTag,
         }),
     },
 }
@@ -46,6 +20,6 @@ export const NavBarLink = Template.bind({})
 NavBarLink.args = {
     path: '#',
 
-    text: 'Link',
+    text: 'Nav Bar Link',
     forceUseDefaulLinkTag: true,
 }
